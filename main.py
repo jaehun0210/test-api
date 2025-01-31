1a4fd98ca4a22e14d5612694848254c100507a1e
jay.kim <jay.kim@edu-poly.com>
2025-01-31 15:51:06 +0900

    -최초커밋

---
+++ main.py
@@ -0,0 +1,12 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+@app.get('/')
+def home():
+	return {"message": "Hello API!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
